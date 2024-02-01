2998ce127ce6fe5cbed20d5454bd13966ee8b9bf
GURU PRASATH <guruprasath@GURUs-MacBook-Pro.local>
2024-02-01 10:43:02 +0530

    second commit

--- src/pages/Home/Navbar.jsx
+++ src/pages/Home/Navbar.jsx
@@ -1,12 +1,109 @@
-export default function Navbar() {
+import { useState, useEffect } from "react";
+import { Link } from "react-scroll";
+
+function Navbar() {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+
+  const closeMenu = () => {
+    setNavActive(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 500) {
+        
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (window.innerWidth <= 1200) {
+      
+    }
+  }, []);
+
   return (
-    <section id="navbar">
-      <h1 className="logo">Arul.</h1>
-      <div className="navbar--links">
+    <nav className={`navbar ${navActive ? "active" : ""}`}>
+      <div>
+        <h1 className="logo-name">Arul.</h1>
+      </div>
+      <a className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav}>
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+      </a>
+      <div className={`navbar--items ${navActive ? "active" : ""}`}>
         <ul>
-          <li>Home</li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="herosection"
+              className="navbar--content"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="project"
+              className="navbar--content"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="Skills"
+              className="navbar--content"
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="contact"
+              className="navbar--content"
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
-    </section>
+    </nav>
   );
 }
+
+export default Navbar;
